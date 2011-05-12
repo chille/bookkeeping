fe6b64ec917d82d3ac21d32ab9315e1bc0721a25
Christian Antila <chille@lab-p4.(none)>
2011-05-13 01:05:45 +0200

    Added HTML base layout header.php, footer.php and CSS files

---
+++ application/views/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<head>
+  <title>Ekonomi</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link type="text/css" rel="stylesheet" media="all" href="/CodeIgniter_2.0.2/application/views/css/default.css" />
+  <link type="text/css" rel="stylesheet" media="all" href="/CodeIgniter_2.0.2/application/views/css/verification.css" />
+  <link type="text/css" rel="stylesheet" media="all" href="/CodeIgniter_2.0.2/application/views/js/jquery-autocomplete/jquery.autocomplete.css" />
+  <link type="text/css" rel="stylesheet" media="all" href="/CodeIgniter_2.0.2/application/views/css/impromptu.css" />
+  <script type="text/javascript" src="/CodeIgniter_2.0.2/application/views/js/jquery-1.6.min.js"></script>
+  <script type="text/javascript" src="/CodeIgniter_2.0.2/application/views/js/jquery-autocomplete/jquery.autocomplete.min.js"></script>
+  <script type="text/javascript" src="/CodeIgniter_2.0.2/application/views/js/jquery-impromptu.3.1.min.js"></script>
+</head>
+<body>
+
+<div id="container">
+  <div id="header">
+    <h1>Bokföring</h1>
+  </div>
+
+  <div id="menu">
+    <h3>Bokföring</h3>
+    <ul>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Verification/MasterLedger">Huvudbok</a></li>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Verification">Verifikationer</a></li>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Verification/Accounts">Konton</a></li>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Verification/Balance">Balansrapport</a></li>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Verification/Result">Resultatrapport</a></li>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Invoice">Fakturor</a></li>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Nordea">Nordea import</a></li>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Verification/test">Test</a></li>
+    </ul>
+
+    <h3>Övrigt</h3>
+    <ul>
+      <li><a href="/CodeIgniter_2.0.2/index.php/Dashboard">Dashboard</a></li>
+      <li><a href="#">Kunder</a></li>
+      <li><a href="#">Leverantörer</a></li>
+      <li><a href="#">Projekt</a></li>
+      <li><a href="#">Tidrapport</a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+  </ul>
+
+  </div>
+
+  <div id="main">
